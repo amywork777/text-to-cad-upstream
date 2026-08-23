NgoQuocViet2001/text-to-cad

Phase B: move the CAD Viewer backend into cadgen, retire the mirror
The viewer's Python backend made 11 imports from cadgen's _internal against 5
public ones, so it was never a separate component -- it shared cadgen's locks,
freshness rules and coordination internals across a package boundary that
existed only on disk. Move it in: viewer/server_py -> cadgen/viewer, and its
tests to tests/python/packages/cadgen/viewer. The dual-import shim and the
PYTHONPATH search that let the two halves find each other are gone.

`pip install cadgen` now gets a working Viewer with no clone: add a `cadgen`
console script dispatching to subcommands (only `viewer` so far -- Phase C
registers the rest), keep `python -m cadgen.viewer` as the equivalent launcher,
and serve the built client from the wheel's _runtime/viewer. Dispatch imports
the target module lazily so `--help` never pays for the OCP import.

Retire the earthtojake/cad-viewer mirror. It was a verbatim copy kept in sync
by a release job; with the backend inside cadgen the viewer is no longer a
self-contained tree to copy, and the rule it enforced no longer holds. Deletes
the sync script, its workflow, the release job, the self-containment test, and
the viewer/packages/cadgen and skills/cad-viewer/scripts/viewer symlinks.

Fixes found while gating this:

- worker_client pointed PYTHONPATH at the cadgen package dir rather than its
  parent, which cannot import cadgen.viewer and leaks every submodule as a bare
  top-level name. Invisible wherever cadgen is installed, so it is pinned by a
  test that runs the import for real.
- cad-python.mjs led its candidate list with viewer/packages/cadgen/src, which
  walks UP -- out of a git worktree and into its parent checkout, binding the
  dev backend to another branch's cadgen. Nearest-first now, with a test that
  every entry stays inside the checkout.
- The release job refused to publish unless skills/cad-viewer/scripts/viewer
  existed -- a path this commit deletes. The wheel-conte

scripts/test/test-python.sh
@@ -28,12 +28,8 @@ done < <("$LIST_SKILLS_SCRIPT")
 
 run_python_unittest "MoveIt2 server Python tests" "tests/python/viewer/moveit2_server" "viewer/moveit2_server"
 
-# The CAD Viewer backend keeps its tests beside the package it covers rather
-# than under tests/, so name the directory explicitly. It owns the only cross-process
-# coverage of the generation lock (test_artifact.py drives a real second process and
-# SIGKILLs it), which is why it must run in CI.
-# packages/cadgen/src is on the path because the viewer server imports cadgen's
-# stdlib-only modules directly (coordination, source_hash) rather than reimplementing
-# them. Without it an installed/editable cadgen from another checkout wins and the
-# coordination package is missing.
-run_python_unittest "CAD Viewer backend Python tests" "viewer/server_py/tests" "viewer" "packages/cadgen/src"
+# The CAD Viewer backend is cadgen.viewer now, so its tests sit with the rest of the
+# cadgen suite. It owns the only cross-process coverage of the generation lock
+# (test_artifact.py drives a real second process and SIGKILLs it), which is why it must
+# run in CI, and it pins the property that the long-lived server imports without OCP.
+run_python_unittest "CAD Viewer backend Python tests" "tests/python/packages/cadgen/viewer" "packages/cadgen/src"

amywork777/text-to-cad-upstream

scripts/test/test-python.sh
@@ -47,15 +47,11 @@ done < <("$LIST_SKILLS_SCRIPT")
 
 run_suite "MoveIt2 server Python tests" "tests/python/viewer/moveit2_server" "viewer/moveit2_server"
 
-# The CAD Viewer backend keeps its tests beside the package it covers rather
-# than under tests/, so name the directory explicitly. It owns the only cross-process
-# coverage of the generation lock (test_artifact.py drives a real second process and
-# SIGKILLs it), which is why it must run in CI.
-# packages/cadgen/src is on the path because the viewer server imports cadgen's
-# stdlib-only modules directly (coordination, source_hash) rather than reimplementing
-# them. Without it an installed/editable cadgen from another checkout wins and the
-# coordination package is missing.
-run_suite "CAD Viewer backend Python tests" "viewer/server_py/tests" "viewer" "packages/cadgen/src"
+# The CAD Viewer backend is cadgen.viewer now, so its tests sit with the rest of the
+# cadgen suite. It owns the only cross-process coverage of the generation lock
+# (test_artifact.py drives a real second process and SIGKILLs it), which is why it must
+# run in CI, and it pins the property that the long-lived server imports without OCP.
+run_suite "CAD Viewer backend Python tests" "tests/python/packages/cadgen/viewer" "packages/cadgen/src"
 
 if [ "${#failed_suites[@]}" -gt 0 ]; then
   printf '\n==> FAILING SUITES (%d)\n' "${#failed_suites[@]}"